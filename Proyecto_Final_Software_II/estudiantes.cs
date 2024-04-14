c5bdb65518ebc288d9fc030c59ebfc7893c4336d
Broncano_Zamata_Erick_Alexander <201801646f@utea.edu.pe>
2024-04-14 15:41:49 -0500

    Se agrega la clase Estudiante al proyecto

---
+++ Proyecto_Final_Software_II/estudiantes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto_Final_Software_II
+{
+    public class estudiantes
+    {
+        public class Student
+        {
+            public int IdEstudiante { get; set; }
+            public string Nombre  { get; set; }
+            public string Apellido { get; set; }
+            public string Grado { get; set; }
+            public string Celular { get; set; }
+        }
+
+    }
+}
