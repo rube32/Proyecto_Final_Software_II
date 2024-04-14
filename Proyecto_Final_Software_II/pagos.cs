aa8119a9440903c6454d8ceca384a9a08b4abf37
Broncano_Zamata_Erick_Alexander <201801646f@utea.edu.pe>
2024-04-14 16:06:21 -0500

    Se agraga la clase PAGO al proyecto

---
+++ Proyecto_Final_Software_II/pagos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto_Final_Software_II
+{
+    public class pagos
+    {
+        // Clase para representar un Pago
+        public class Payment
+        {
+            public int IdPago { get; set; }
+            public int IdEstidiante { get; set; }
+            public decimal Cantidad { get; set; }
+            public DateTime Fecha_de_pago
+            { get; set; }
+        }
+    }
+}
