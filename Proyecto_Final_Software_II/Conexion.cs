5c3c4f6d67bcfd373791ff940f78712de3298825
Rodriguez_Bejar_Sebastian <Sabnait@hotmail.com>
2024-04-15 15:32:56 -0500

    Se crea la clase conexion hacia la base de datos BDEscuela

---
+++ Proyecto_Final_Software_II/Conexion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Proyecto_Final_Software_II
+{
+    class Conexion
+    {
+       public static SqlConnection Conectar()
+        {
+            SqlConnection cn = new SqlConnection("SERVER = DESKTOP-VIITSAH\SEBASTIAN;DATABASE;BdEscuela;Integrate Security=true");
+            cn.Open();
+            return cn;
+        }
+
+    } 
+    
+    
+}
