cd0770b38a57378342740608ddf2ea97ab4685bc
Jancco_Quehuarucho_Carlos <201913888f@utea.edu.pe>
2024-04-16 14:48:35 -0500

    Se implemente WebInterfaz Estudiante y Pago

---
+++ WebInterface/Estudiante.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Web.UI.WebControls;
+using WebApp.WcfService;
+
+namespace WebApp
+{
+    public partial class Estudiantes : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                CargarEstudiantes();
+            }
+        }
+
+        private void CargarEstudiantes()
+        {
+            var client = new Estudiante();
+
+            try
+            {
+                var estudiantes = client.ObtenerListaEstudiantes();
+                gvEstudiantes.DataSource = estudiantes;
+                gvEstudiantes.DataBind();
+            }
+            catch (Exception ex)
+            {
+                // Manejar error
+            }
+            finally
+            {
+                client.Close();
+            }
+        }
+
+        protected void gvEstudiantes_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "VerDetalles")
+            {
+                int rowIndex = int.Parse(e.CommandArgument.ToString());
+                int idEstudiante = int.Parse(Estudiantes.DataKeys[rowIndex]["Id"].ToString());
+                Response.Redirect($"EstudianteDetalle.aspx?id={idEstudiante}");
+            }
+        }
+    }
+}
---
+++ WebInterface/Pagos .cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Web.UI;
+// Namespace del cliente generado para el servicio WCF
+
+namespace WebApp
+{
+    public partial class Pagos : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+               
+            }
+        }
+
+        protected void btnRegistrarPago_Click(object sender, EventArgs e)
+        {
+   
+            var client = new Pago();
+
+            try
+            {
+             
+                int idEstudiante = Convert.ToInt32(Estudiante.SelectedValue);
+                double montoPago = Convert.ToDouble(Pago.Text);
+               
+                client.RegistrarPago(idEstudiante, montoPago);
+              
+                lblMensaje.Text = "El pago se ha registrado correctamente.";
+            }
+            catch (Exception ex)
+            {
+               
+                lblMensaje.Text = "Error al registrar el pago: " + ex.Message;
+            }
+            finally
+            {
+                
+                client.Close();
+            }
+        }
+    }
+
+    public partial class EstadoCuenta : Page
+    {
+        protected void btnGenerarReporte_Click(object sender, EventArgs e)
+        {
+            
+            var client = new Pago();
+
+            try
+            {
+                
+                int idEstudiante = Convert.ToInt32(ddlEstudiantes.SelectedValue);
+                
+                var estadoCuenta = client.ObtenerEstadoCuenta(idEstudiante);
+                // Mostrar el estado de cuenta en algún control de la interfaz de usuario 
+                gvEstadoCuenta.DataSource = estadoCuenta;
+                gvEstadoCuenta.DataBind();
+            }
+            catch (Exception ex)
+            {
+                // Manejar cualquier excepción y mostrar un mensaje de error
+                lblMensaje.Text = "Error al generar el estado de cuenta: " + ex.Message;
+            }
+            finally
+            {
+                // Siempre cerrar el cliente del servicio WCF
+                client.Close.();
+            }
+        }
+    }
+}
